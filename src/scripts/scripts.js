5d0d408441f29202c29d24cfa720a6f639969736
Cleiton T. Machado <cleiton.t.machado@gmail.com>
2025-09-23 10:34:25 -0300

    concluído aulas do mês 1

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,16 @@
+console.log('Olá do meu Portfólio!'); 
+const saudacaoBotao = document.getElementById('saudacaoBtn'); 
+const saudacaoMensagem = document.getElementById('saudacaoMensagem'); 
+if (saudacaoBotao) { 
+    //Verifica se o botão existe na página 
+    saudacaoBotao.addEventListener('click', function() { 
+    saudacaoMensagem.textContent = 
+        saudacaoMensagem.textContent === '' ? 'em-vindo(a) ao meu portfólio! Espero que goste.' 
+        : ''; //usamos um operador ternário para alternar a mensagem exibindo e ocultando;
+    // Você pode adicionar mais lógica aqui, como mudar a cor do texto, etc. 
+    });	
+} 
+
 const addClass = (className, id)=>{
     const element = document.getElementById(id);
     console.log(className, id, element)
@@ -14,5 +27,7 @@ const removeClass = (className, id)=>{
         element.classList.remove(className);
     }
 }
+
+
 
   
