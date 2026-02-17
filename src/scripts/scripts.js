d00855134a23706fe54a9a002afac88cd9c70d2e
Cleiton T. Machado <cleiton.t.machado@gmail.com>
2026-02-17 13:20:22 -0300

    feito revisão do código em conformidade com a apostila do curso

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,11 +5,22 @@ if (saudacaoBotao) {
     //Verifica se o botão existe na página 
     saudacaoBotao.addEventListener('click', function() { 
     saudacaoMensagem.textContent = 
-        saudacaoMensagem.textContent === '' ? 'em-vindo(a) ao meu portfólio! Espero que goste.' 
+        saudacaoMensagem.textContent === '' ? 'Bem-vindo(a) ao meu portfólio! Espero que goste.' 
         : ''; //usamos um operador ternário para alternar a mensagem exibindo e ocultando;
     // Você pode adicionar mais lógica aqui, como mudar a cor do texto, etc. 
     });	
 } 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuMobile = document.querySelector("#menu-mobile");
+    const menuNavegacao = document.querySelector("#nav");
+ 
+    menuMobile.addEventListener("click", function() {
+        menuNavegacao.classList.toggle("ativo");
+    });
+});
+
+
 
 const addClass = (className, id)=>{
     const element = document.getElementById(id);
